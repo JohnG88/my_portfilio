9e3d89ce4d5c53f0aba1fecfff92633caa10f66e
John Gutierrez <djterez88@gmail.com>
2022-04-06 21:11:55 -0700

    Added functionality to view individual project pages

--- portfoliowebsite/base/views.py
+++ portfoliowebsite/base/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
-from .models import Project
+from .models import Project, Skill
 
 # Create your views here.
 
 def homePage(request):
     project = Project.objects.all()
+    detailed_skills = Skill.objects.exclude(body='')
+    skills = Skill.objects.filter(body='')
 
-    context = {'project': project}
+    context = {'project': project, 'skills': skills, 'detailed_skills': detailed_skills}
     return render(request, 'base/home.html', context)
+
+def projectPage(request, pk):
+
+    context = {}
+    return render(request, 'base/project.html', context)
