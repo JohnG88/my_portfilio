f22d6c150a6df789ba60ea025bdf0f70fcc65a31
John Gutierrez <djterez88@gmail.com>
2022-04-08 13:04:47 -0700

    Added messaging functionality, but it is only one way

--- portfoliowebsite/base/views.py
+++ portfoliowebsite/base/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Project, Skill
+from .models import Project, Skill, Message
 from .forms import ProjectForm
 
 # Create your views here.
@@ -43,3 +43,18 @@ def editProject(request, pk):
 
     context = {'form': form}
     return render(request, 'base/project_form.html', context)
+
+def inboxPage(request):
+    # messages that haven't been read top priority
+    inbox = Message.objects.all().order_by('is_read')
+
+    context = {'inbox': inbox}
+    return render(request, 'base/inbox.html', context)
+
+def messagePage(request, pk):
+    message = Message.objects.get(id=pk)
+    message.is_read = True
+    message.save()
+
+    context = {'message': message}
+    return render(request, 'base/message.html', context)
